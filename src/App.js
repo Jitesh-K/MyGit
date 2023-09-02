d46c1762bea9f3b5b894f54484b4dafa3b9ed563
Jitesh Kumar <jiteshj.kumar@gmail.com>
2023-09-02 16:28:17 +0530

    added feeds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Box } from '@chakra-ui/layout';
 import { React } from 'react';
+import { Feed } from './feed';
 
 export const App = () => {
-    return (<h1>Chakra Ui Setup Complete</h1>)
+    return <Box width="100vw" minHeight={'100vh'} bg={'gray.100'}><Feed /></Box>
 }
