0199d384dc5ce380d1e22e9bba4acae1ef530ba2
Jitesh Kumar <jiteshj.kumar@gmail.com>
2023-09-02 22:31:00 +0530

    refactor code

--- src/feed.js
+++ src/feed.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Box, Flex } from "@chakra-ui/layout"
-import { Brand } from './components/brand';
-import { PageHeader } from './components/page-header';
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+import { PageHeader } from "./components/page-header";
 
 export const Feed = () => {
-    return <Box maxWidth={'1200px'} mx={'auto'}>
-        <Flex justifyContent={'space-between'} alignItems={'center'} pt='15px'>
-            <Brand />
-            <PageHeader />
-        </Flex>
-    </Box>;
-}
+  return (
+    <Box maxWidth={"1200px"} mx={"auto"}>
+      <PageHeader />
+    </Box>
+  );
+};
