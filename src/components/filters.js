597951d6a93d13ded0482de30f28e5f129d990d9
Jitesh Kumar <jiteshj.kumar@gmail.com>
2023-09-03 11:58:54 +0530

    added actions and view filter

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,34 +1,48 @@
 import React from "react";
 import {
   Button,
-  Flex,
+  HStack,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Select,
 } from "@chakra-ui/react";
-import { FaAngleDown } from "react-icons/fa";
+import { FaCalendar, FaList, FaTable } from "react-icons/fa";
 import languages from "../data/languages.json";
 
 export const Filters = () => {
   return (
-    <Flex>
-      <Select>
+    <HStack>
+      <Select bg={"white"}>
         {languages.map((language) => (
           <option value={language.value}>{language.title}</option>
         ))}
       </Select>
-      {/* <Menu>
-        <MenuButton as={Button} leftIcon={<FaAngleDown />}>
-          Languages
+      <Menu>
+        <MenuButton
+          as={Button}
+          bg={"white"}
+          borderWidth={'thin'}
+          pl={'5'}
+          pr={'20'}
+          fontWeight={"400"}
+          leftIcon={<FaCalendar />}
+        >
+          Actions
         </MenuButton>
         <MenuList>
-          {languages.map((x) => (
-            <MenuItem>{x.title}</MenuItem>
-          ))}
+          <MenuItem>Download</MenuItem>
+          <MenuItem>Create a Copy</MenuItem>
+          <MenuItem>Mark as Draft</MenuItem>
+          <MenuItem>Delete</MenuItem>
+          <MenuItem>Attend a Workshop</MenuItem>
         </MenuList>
-      </Menu> */}
-    </Flex>
+      </Menu>
+      <HStack spacing={0} borderWidth={'thin'} ml>
+        <Button leftIcon={<FaTable />} bg={'white'}>Grid</Button>
+        <Button leftIcon={<FaList />} bg={'white'}>List</Button>
+      </HStack>
+    </HStack>
   );
 };
