51057d3eb9c6f5c507d45d558d5f46944f705102
Jitesh Kumar <jiteshj.kumar@gmail.com>
2023-09-09 18:52:29 +0530

    updated project name

--- src/components/brand.js
+++ src/components/brand.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Box, Flex, Heading, Text } from "@chakra-ui/layout"
-import { Image } from '@chakra-ui/image';
+import { Box, Flex, Heading, Text, HStack } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
 
 export const Brand = () => {
-    return (<Flex alignItems={'center'}>
-        <Image boxSize='80px' src='/logo.svg' />
-        <Box ml='10px'>
-            <Heading fontSize={'24px'}>TopGit</Heading>
-            <Text color={'gray.600'}>Most starred projects on github</Text>
-        </Box>
-    </Flex>);
-}
+  return (
+    <Flex alignItems={"center"}>
+      <Image boxSize="80px" src="/logo.svg" />
+      <Box ml="10px">
+        <Heading fontSize={"24px"}>MyGit</Heading>
+        <HStack>
+          <Text color={"gray.600"}>
+            Custom view listing my Repositories: Jitesh-K
+          </Text>
+        </HStack>
+      </Box>
+    </Flex>
+  );
+};
